1778eb54cbcef240760c81c3c2565145f5502a63
redwallhp <mcredwallhp@gmail.com>
2015-08-17 22:11:52 +1000

    Added support for conditional plump radius, cleaned up code, fixed non-functional ore plump.

--- src/com/github/barneygale/PlumpXP/Configuration.java
+++ src/com/github/barneygale/PlumpXP/Configuration.java
@@ -1,5 +1,7 @@
 package com.github.barneygale.PlumpXP;
 
+import java.util.List;
+
 public class Configuration {
 	private final PlumpXP plugin;
 	public double PLUMP_MULTIPLIER;
@@ -7,6 +9,10 @@ public class Configuration {
 	public boolean PLAYER_OVERRIDE;
 	public double PLAYER_MULTIPLIER;
 	public double ORE_MULTIPLIER;
+    public boolean PLUMP_OUTSIDE_RADIUS;
+    public double PLUMP_RADIUS;
+    public List<Double> PLUMP_RADIUS_CENTER;
+    public List<String> PLUMP_RADIUS_WORLDS;
 	
 	public Configuration(PlumpXP instance) {
 		plugin = instance;
@@ -21,6 +27,10 @@ public class Configuration {
 		PLAYER_OVERRIDE = plugin.getConfig().getBoolean("player-override");
 		PLAYER_MULTIPLIER = plugin.getConfig().getDouble("player-multiplier");
 		ORE_MULTIPLIER = plugin.getConfig().getDouble("ore-multiplier");
+        PLUMP_OUTSIDE_RADIUS = plugin.getConfig().getBoolean("plump-outside-radius.enabled");
+        PLUMP_RADIUS = plugin.getConfig().getDouble("plump-outside-radius.radius");
+        PLUMP_RADIUS_CENTER = plugin.getConfig().getDoubleList("plump-outside-radius.centerpoint");
+        PLUMP_RADIUS_WORLDS = plugin.getConfig().getStringList("plump-outside-radius.worlds");
 	}
 
 }
--- src/com/github/barneygale/PlumpXP/PlumpXPListener.java
+++ src/com/github/barneygale/PlumpXP/PlumpXPListener.java
@@ -1,16 +1,21 @@
 package com.github.barneygale.PlumpXP;
 
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerExpChangeEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 public class PlumpXPListener implements Listener {
     public final PlumpXP plugin;
@@ -20,26 +25,34 @@ public class PlumpXPListener implements Listener {
 
     @EventHandler
     public void onEntityDeath(EntityDeathEvent event) {
-         Entity target = event.getEntity();
-         Player attacker = getAttacker(target.getLastDamageCause());
-         int xp = event.getDroppedExp();
-         
-         if(event instanceof PlayerDeathEvent) {
-                handlePlayerDeath((PlayerDeathEvent) event,attacker, xp);
-                return;
-         }
 
-         //not killed by player
-         if(attacker == null) {
-                return;
-            }
-         handleMonsterDeath(event, xp);
+        Entity target = event.getEntity();
+        Player attacker = getAttacker(target.getLastDamageCause());
+        int xp = event.getDroppedExp();
+
+        if (attacker != null) {
+            if (plugin.config.PLUMP_OUTSIDE_RADIUS && !playerIsOutsideRadius(attacker)) return;
+        }
+         
+        if (event instanceof PlayerDeathEvent) {
+            handlePlayerDeath((PlayerDeathEvent) event, attacker, xp);
+            return;
         }
 
-    public void onBlockBreak(BlockBreakEvent event) {
-    	int xp = event.getExpToDrop();
+        //not killed by player
+        if (attacker == null) {
+            return;
+        }
 
-    	handleBlockBreak((BlockBreakEvent) event, xp);
+        handleMonsterDeath(event, xp);
+
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        if (plugin.config.PLUMP_OUTSIDE_RADIUS && !playerIsOutsideRadius(event.getPlayer())) return;
+        int xp = event.getExpToDrop();
+        handleBlockBreak(event, xp);
     }
 
     private void handleMonsterDeath(EntityDeathEvent event, int exp) {
@@ -49,13 +62,13 @@ public class PlumpXPListener implements Listener {
         }
         else {
             if (plugin.config.PLUMP_MULTIPLIER > 0) {
-            	exp *= plugin.config.PLUMP_MULTIPLIER;
+                exp *= plugin.config.PLUMP_MULTIPLIER;
             }
         }
         event.setDroppedExp(exp);
     }
 
-    private void handlePlayerDeath(PlayerDeathEvent event,Player attacker, int exp) {
+    private void handlePlayerDeath(PlayerDeathEvent event, Player attacker, int exp) {
         //killed by another player
         if ((attacker != null) && (plugin.config.PLAYER_OVERRIDE)) {
             exp *= plugin.config.PLAYER_MULTIPLIER;
@@ -64,29 +77,43 @@ public class PlumpXPListener implements Listener {
     }
 
     private Player getAttacker(EntityDamageEvent attacker) {
-        if((attacker == null) || (!(attacker instanceof EntityDamageByEntityEvent))){
+        if ((attacker == null) || (!(attacker instanceof EntityDamageByEntityEvent))){
             return null;
         }
 
         Player p = null;
         Entity damager = ((EntityDamageByEntityEvent) attacker).getDamager();
 
-        if(damager instanceof Projectile) {
+        if (damager instanceof Projectile) {
             Projectile projectile = (Projectile) damager;
-            if(projectile.getShooter() instanceof Player) {
+            if (projectile.getShooter() instanceof Player) {
                 p = (Player) projectile.getShooter();
             }
         } 
-        else if(damager instanceof Player) {
+        else if (damager instanceof Player) {
             p = (Player)damager;
         }
         return p;
     }
     
     private void handleBlockBreak(BlockBreakEvent event, int exp) {
-    	if (plugin.config.ORE_MULTIPLIER > 0) {
-    		exp *= plugin.config.ORE_MULTIPLIER;
-    	}
-    	event.setExpToDrop(exp);
+        if (plugin.config.ORE_MULTIPLIER > 0) {
+            exp *= plugin.config.ORE_MULTIPLIER;
+        }
+        event.setExpToDrop(exp);
     }
+
+    private boolean playerIsOutsideRadius(Player player) {
+
+        if (!plugin.config.PLUMP_RADIUS_WORLDS.contains(player.getWorld().getName())) return false; //check if world is enabled
+
+        double centerX = plugin.config.PLUMP_RADIUS_CENTER.get(0);
+        double centerZ = plugin.config.PLUMP_RADIUS_CENTER.get(1);
+        Location loc = player.getLocation();
+
+        double distance = Math.pow(centerX - loc.getX(), 2) + Math.pow(centerZ - loc.getZ(), 2); //Euclidean
+        return distance > Math.pow(plugin.config.PLUMP_RADIUS, 2);
+
+    }
+
 }
